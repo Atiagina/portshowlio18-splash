0700515071383a583b2e62db6f03d27b061e2dfb
Anna <atiagina@gmail.com>
2018-04-17 22:27:20 -0700

    New theme for 2018
    all new files in one commit

--- front-page.php
+++ front-page.php
@@ -10,7 +10,14 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="SCCA WEB TEAM">
-
+<style>
+	@font-face {
+	
+	font-family: HalisR;
+    src: url('<?php bloginfo('template_url');?>/fonts/HalisR-Medium.otf');
+}
+	
+	</style>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <link rel="stylesheet" href="<?php bloginfo('template_url');?>/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
@@ -20,65 +27,55 @@
 </head>
 
 <body>
-	<video poster="<?php bloginfo('template_url');?>/poster.jpg" id="bgvid" playsinline autoplay muted loop>
-		<source src="<?php bloginfo('template_url');?>/background.mp4" type="video/webm">
-		<source src="<?php bloginfo('template_url');?>/background.mp4" type="video/mp4">
-	</video>
+	
 
-	<div class='shape'>
-	<div class="container">
-
-
-	<div>
-		<div class="col-xs-6" style="max-width:200px;">
-			<img class="logo" src="<?php bloginfo('template_url');?>/logo.svg">
+	<div class="backgrounddiv">
+		
+	<div class="infocontainer">
+<div id="wrapper">
+<div id="helper">
+	<div class="yellowblock">
+		<div class="logoblock">
+			<img class="logo" src="<?php bloginfo('template_url');?>/img/logo_placeholder.svg" >
 		</div>
-		<div class="col-xs-6">
-			<h1>SCCA</h1><p class='twenty'>2018</p>
+		
+		<div class="descriptionblock"> <p id='description'><?php the_field('description'); ?></p>
 		</div>
-		<div class="col-md-12 col-s-12 col-xs-12" style="height:10vh;"> <!--height auto replaces style="height: 15vh;"-->
-			
-			<p id='description'><?php the_field('description'); ?></p>
-		</div>
-	</div>
-
-	<hr>
-
-	<div class="">
-
-		<div class="col-md-12 col-s-12 col-xs-12">
-			<h2><?php the_field('day_1_title'); ?></h2>
-			<h3><?php the_field('day_1_date'); ?></h3>
-			<h3 class="time"><?php the_field('day_1_time'); ?></h3>
-		</div>
-
-	<hr>
-
-		<div class="col-md-12 col-s-12 col-xs-12">
-			<h2><?php the_field('day_2_title'); ?></h2>
-			<h3><?php the_field('day_2_date'); ?></h3>
-			<h3 class="time"><?php the_field('day_2_time'); ?></h3>
-		</div>
-
-	</div>
-
-
-	<hr>
-
-		<div class="footer2">
-			<div class="col-md-5 col-xs-5">
-				<ul class="social">
+		
+		<div class="socialblock">
+			<ul class="social">
 					<li class="icons"><a href="<?php the_field('facebook_link'); ?>" target="_blank" title="Facebook"><i class="fa fa-facebook fa-2x black"></i></a></li>
 					<li class="icons"><a href="<?php the_field('ig_link'); ?>" target="_blank" title="Instagram"><i class="fa fa-instagram fa-2x black"></i></a></li>
 					<li class="icons"><a href="<?php the_field('twitter_link'); ?>" target="_blank" title="Twitter"><i class="fa fa-twitter fa-2x black"></i></a></li>
 				</ul>
-			</div>
-
-			<div class="col-md-7 col-xs-7">
-				<h4>Seattle Central Creative Academy</h4>
-				<h4>1701 Broadway Ave Seattle, WA 98122</h4>
-			</div>
 		</div>
+	</div>
+
+	<div class="blockrow">
+		<div class="date profesh">
+			<img src="<?php bloginfo('template_url');?>/img/night1.svg" alt="scca">
+			<h3><?php the_field('day_1_date'); ?></h3>
+			<h3 class="time"><?php the_field('day_1_time'); ?></h3>
+		</div>
+		<div class="scca">
+			<img src="<?php bloginfo('template_url');?>/img/scca.svg" alt="scca">
+		</div>
+		</div>
+		
+		<div class="blockrow">
+		<div class="date family">
+			<img src="<?php bloginfo('template_url');?>/img/night2.svg" alt="scca">
+			<h3><?php the_field('day_2_date'); ?></h3>
+			<h3 class="time"><?php the_field('day_2_time'); ?></h3>
+		</div>
+			<div class="address">
+				
+				<h4>1701 Broadway Ave Seattle, WA 98122</h4>
+		</div>
+		</div>
+	</div>
+		</div> <!-- end of wrapper -->
+
 	</div>
 	</div>
 	<script>
